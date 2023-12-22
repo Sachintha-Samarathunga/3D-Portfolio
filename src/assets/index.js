b8019a6e230eea7987b1e9ac75b8a16ec0e41c24
kashyapa01 <98406068+Sachintha-Samarathunga@users.noreply.github.com>
2023-12-23 02:04:37 +0530

    update changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import ecommerce from "./ecommerce.png";
 
 export {
   logo,
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  ecommerce
 };
